c07acf7eaef2a88bb61ac36a98b8757d30859bd4
kund20 <nguyentiendat6849@gmail.com>
2024-09-24 16:05:13 +0700

    update lab2

--- type_hint/C.php
+++ type_hint/C.php
@@ -4,7 +4,7 @@ require_once 'I.php'; // Nhúng interface I
 
 class C implements I {
     public function f(): void {
-        echo "This is function f from class C\n"; // Triển khai phương thức f()
+        echo "This is function f from class C\n"; 
     }
 }
 
--- type_hint/I.php
+++ type_hint/I.php
@@ -1,5 +1,6 @@
 <?php
 
+
 // interface
 interface I {
     public function f(); // Định nghĩa phương thức f() trong interface
