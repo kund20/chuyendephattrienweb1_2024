6cf1fe55d89507efce313f24ee3d130ef0d513b7
Kund20 <nguyentiendat6830@gmail.com>
2024-10-15 15:00:04 +0700

    update lab4

---
+++ front/3069/3069.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>News Section</title>
+    <link rel="stylesheet" href="css/3069.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!--<link rel="stylesheet" href="less/style.less">
+    <script src="https://cdn.jsdelivr.net/npm/less@4.1.1"></script>
+-->
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 main-content">
+                <div
+                    style="margin:0 0 10px 0;padding:10px 35px;background-color:#ffffd2;color:#555;font-size:16px;text-align:center;">
+                    <strong>Dev License:</strong> This installation of WHMCS is running under a Development License and
+                    is
+                    not
+                    authorized to be used for production use. Please report any cases of abuse to abuse@whmcs.com
+                </div>
+            </div>
+            <h2>News</h2>
+            <div class="announcement-sigle">
+                <h3>
+                    <span class="label label-default">May 6th</span>
+                    <a href="">Welcome To WHMCS</a>
+                </h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae deleniti, reiciendis molestias id
+                    harum,
+                    magnam nulla quaerat quod praesentium, quos sequi? Laudantium, nisi tempora accusamus, aliquam
+                    quisquam
+                    vel. Nihil, laborum. Thank you for choosing WHMCS Template !Welcome to WHMCS! You have made a great
+                    choice and we want to help you get up ... </p>
+                <a href="" class="label label-warning">Read more >></a>
+
+            </div>
+            <div class="announcement-sigle">
+                <h3>
+                    <span class="label label-default">Sept 6th</span>
+                    <a href="">Thank you for choosing WHMCS 7 Templates !</a>
+                </h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae deleniti, reiciendis molestias id
+                    harum,
+                    magnam nulla quaerat quod praesentium, quos sequi? Laudantium, nisi tempora accusamus, aliquam
+                    quisquam
+                    vel. Nihil, laborum. Thank you for choosing WHMCS Template !Welcome to WHMCS! You have made a great
+                    choice and we want to help you get up ... </p>
+                <a href="" class="label label-warning">Read more >></a>
+
+            </div>
+        </div>
+        <footer>
+            Powered by <a href="#">WHMCompleteSolution</a>
+        </footer>
+
+</body>
+
+</html>
