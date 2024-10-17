0ae843cfab36aa2498bd1199e62e9452959928d4
Kund20 <nguyentiendat6830@gmail.com>
2024-10-17 13:09:41 +0700

    update lab4

--- front/3069/3069.php
+++ front/3069/3069.php
@@ -7,7 +7,6 @@
     <title>News Section</title>
     <link rel="stylesheet/less" type="text/css" href="less/3069.less">
     <link rel="stylesheet" href="css/3069.css">
-
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
