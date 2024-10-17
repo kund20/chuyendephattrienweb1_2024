3713580e4df1156c8eef2f774956190493802a59
Kund20 <nguyentiendat6830@gmail.com>
2024-10-17 11:17:23 +0700

    update lab4

--- front/3069/3069.php
+++ front/3069/3069.php
@@ -5,60 +5,65 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>News Section</title>
+    <link rel="stylesheet/less" type="text/css" href="less/3069.less">
     <link rel="stylesheet" href="css/3069.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <!--<link rel="stylesheet" href="less/style.less">
-    <script src="https://cdn.jsdelivr.net/npm/less@4.1.1"></script>
--->
+
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 
 <body>
     <div class="container">
         <div class="row">
-            <div class="col-xs-12 main-content">
-                <div
-                    style="margin:0 0 10px 0;padding:10px 35px;background-color:#ffffd2;color:#555;font-size:16px;text-align:center;">
+            <div class="col-12 main-content">
+                <div class="dev-license">
                     <strong>Dev License:</strong> This installation of WHMCS is running under a Development License and
-                    is
-                    not
-                    authorized to be used for production use. Please report any cases of abuse to abuse@whmcs.com
+                    is not authorized to be used for production use. Please report any cases of abuse to abuse@whmcs.com
                 </div>
             </div>
-            <h2>News</h2>
-            <div class="announcement-sigle">
+        </div>
+        <!-- Start News Section -->
+        <h2>News</h2>
+        <div class="news-container">
+            <!-- News item 1 -->
+            <div class="announcement-single">
                 <h3>
                     <span class="label label-default">May 6th</span>
                     <a href="">Welcome To WHMCS</a>
                 </h3>
+                
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae deleniti, reiciendis molestias id
-                    harum,
-                    magnam nulla quaerat quod praesentium, quos sequi? Laudantium, nisi tempora accusamus, aliquam
-                    quisquam
-                    vel. Nihil, laborum. Thank you for choosing WHMCS Template !Welcome to WHMCS! You have made a great
-                    choice and we want to help you get up ... </p>
-                <a href="" class="label label-warning">Read more >></a>
-
+                    harum, magnam nulla quaerat quod praesentium, quos sequi? Laudantium, nisi tempora accusamus,
+                    aliquam quisquam vel. Nihil, laborum. Thank you for choosing WHMCS Template !Welcome to WHMCS! You
+                    have made a great choice and we want to help you get up ... <a href="" class="label label-warning">Read more >></a></p>
+                
+                
             </div>
-            <div class="announcement-sigle">
+
+            <!-- News item 2 -->
+            <div class="announcement-single">
                 <h3>
                     <span class="label label-default">Sept 6th</span>
                     <a href="">Thank you for choosing WHMCS 7 Templates !</a>
                 </h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae deleniti, reiciendis molestias id
-                    harum,
-                    magnam nulla quaerat quod praesentium, quos sequi? Laudantium, nisi tempora accusamus, aliquam
-                    quisquam
-                    vel. Nihil, laborum. Thank you for choosing WHMCS Template !Welcome to WHMCS! You have made a great
-                    choice and we want to help you get up ... </p>
-                <a href="" class="label label-warning">Read more >></a>
-
+                <p>Thank you for choosing WHMCS Template !Welcome to WHMCS! You have made a great choice and we want to
+                    help you get up and running as quickly as possible.This is a sample announcement. Announcements are
+                    a great way to keep your customers informed about news and special offers. You can edit or delete
+                    this announcement by logging into the admin area ... <a href="" class="label label-warning">Read more >></a></p>
+                
             </div>
+
+            <!-- Add more news items if needed -->
         </div>
+        <!-- End News Section -->
+
         <footer>
             Powered by <a href="#">WHMCompleteSolution</a>
         </footer>
+    </div>
 
+
+    </div>
 </body>
 
 </html>
---
+++ front/3184/3184.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Just Keep On Walking</title>
+    <link rel="stylesheet" href="css/3184.css">
+    <link rel="stylesheet/less" type="text/css" href="less/3184.less">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <!-- Main Content -->
+            <div class="col-md-8">
+                <div class="main-content">
+                    <div class="post-image">
+                        <img src="pic/Blog-standard.jpg" alt="Walking Shoes" class="img-fluid">
+                        <!-- class img-fluid cho hình ảnh -->
+                    </div>
+                    <div class="post-meta">
+                        <span>Accessories, Biking</span>
+                        <span>03/10/2017</span>
+                    </div>
+                    <h1 class="post-title">Just Keep On Walking</h1>
+                    <p class="post-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et
+                        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                        aliquip
+                        ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                        dolore eu
+                        fugiat nulla pariatur. Excepteur sint occaeca...
+                    </p>
+                    <div class="post-footer">
+                        <span>By admin</span>
+                        <span>&#128172; 2 Comments</span>
+                        <span>&hearts; 9 Likes</span>
+                        <a href="#" class="read-more"><span>Read more &#62;</span></a>
+                    </div>
+                </div>
+            </div>
+            <!-- Sidebar -->
+            <div class="col-md-4">
+                <div class="sidebar">
+                    <div class="author">
+                        <h4>ABOUT AUTHOR:</h4>
+                        <img src="pic/avatar.jpg" alt="Author" class="author-image img-fluid">
+                        <!-- class img-fluid cho hình ảnh -->
+                        <p class="author-description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore
+                            et dolore magna aliqua.
+                        </p>
+                    </div>
+                    <div class="categories">
+                        <h4>CATEGORIES</h4>
+                        <ul>
+                            <li>Accessories</li>
+                            <li>Biking</li>
+                            <li>Blouses</li>
+                            <li>Hiking</li>
+                            <li>Outerwear</li>
+                            <li>Running</li>
+                            <li>Swimming</li>
+                        </ul>
+                    </div>
+                    <div class="tags">
+                        <h4>TAGS</h4>
+                        <p>Backpacks / Black / Casual / Classic / Cotton / Elegant / Fashion / Footwear / Outerwear /
+                            Rubber /
+                            Sweaters / Swim</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
